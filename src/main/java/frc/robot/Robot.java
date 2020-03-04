c4f8bd28020d30bf1474387fee53c98ea515a430
FRC7853 <frc.team7853@gmail.com>
2020-03-04 15:46:47 -0500

    Testing LED

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Victor;
@@ -22,29 +23,18 @@ public class Robot extends TimedRobot {
   private final Victor b_leftMotor = new Victor(1);
   private final Victor f_rightMotor = new Victor(2);
   private final Victor b_rightMotor = new Victor(3);
-  private final Victor s_leftMotor = new Victor(4);
-  private final Victor c_Motor = new Victor(5);
-  private final Victor s_rightMotor = new Victor(6);
-  private final Victor i_Motor = new Victor(7);
-  private final double speed = 0.3;
-  //private final SpeedControllerGroup r_motors = new SpeedControllerGroup(f_rightMotor, b_rightMotor);
-  //private final SpeedControllerGroup l_motors = new SpeedControllerGroup(f_leftMotor, b_leftMotor);
-  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(r_motors, l_motors);
-  //private final Joystick m_stick = new Joystick(0);
+  private final Spark blinkin = new Spark(4);
+  private final SpeedControllerGroup r_motors = new SpeedControllerGroup(f_rightMotor, b_rightMotor);
+  private final SpeedControllerGroup l_motors = new SpeedControllerGroup(f_leftMotor, b_leftMotor);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(r_motors, l_motors);
+  private final Joystick m_stick = new Joystick(0);
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    //m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
-    f_leftMotor.set(speed);
-    f_rightMotor.set(speed);
-    b_leftMotor.set(speed);
-    b_rightMotor.set(speed);
-    s_leftMotor.set(speed);
-    s_rightMotor.set(speed);
-    c_Motor.set(speed);
-    i_Motor.set(speed);
+    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    blinkin.set(0.9);
   }
 }
